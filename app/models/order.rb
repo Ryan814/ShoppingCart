795c6585e2c9d2f83bc197f7093a0b876ad53691
Ryan814 <rytg156@gmail.com>
2019-03-11 10:42:02 +0800

    add form for new order on cart list page

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   validates_presence_of :name, :address, :phone, :payment_status, :shipping_status
 
   belongs_to :user
-  has_many: :order_items, dependent: :destroy
-  has_many: :products, through: :order_items
+  has_many :order_items, dependent: :destroy
+  has_many :products, through: :order_items
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   end
 
   resource :cart
+  resource :orders
 
   namespace :admin do
     root "products#index"
